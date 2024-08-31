dd5bf95cf2ee96125c332d25f899c5281c0bf337
rupalikorade <rupalikorade1563@gmail.com>
2024-08-31 15:57:20 +0530

    if else condition on images

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function changeImage(num){
             
     const mainImage = document.getElementById('main-img');
-   
+    if(num==1){
+        mainImage.src = "./images/freestocks-L5DxWLmywmM-unsplash.jpg";
+    }else if(num==2){
+        mainImage.src = "./images/william-hook-9e9PD9blAto-unsplash.jpg";
+    }else if(num==3){
+        mainImage.src = "./images/priscilla-du-preez-tBypczR1CFI-unsplash.jpg";
+    }else if(num==4){
+        mainImage.src = "./images/william-hook-9e9PD9blAto-unsplash (1).jpg";
+    }else if(num==5){
+        mainImage.src = "./images/priscilla-du-preez-BOUdudmAnk4-unsplash.jpg";
+    }
 }
